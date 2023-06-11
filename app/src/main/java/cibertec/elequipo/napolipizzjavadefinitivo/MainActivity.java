75c08eba1b0fedf7636edec0f67df6af3bb353dc
Diego-coder2525 <forgam2525@outlook.es>
2023-06-11 15:27:03 -0500

    Initial commit

---
+++ app/src/main/java/cibertec/elequipo/napolipizzjavadefinitivo/MainActivity.java
@@ -0,0 +1,14 @@
+package cibertec.elequipo.napolipizzjavadefinitivo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
