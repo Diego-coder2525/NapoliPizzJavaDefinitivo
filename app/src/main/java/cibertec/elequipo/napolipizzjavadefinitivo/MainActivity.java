96bc907e04d8d48ccf5d5f29106662a8ffb6ff10
ikigaia1998 <i202117849@cibertec.edu.pe>
2023-06-11 20:22:19 -0500

    Respaldo2 xd

--- app/src/main/java/cibertec/elequipo/napolipizzjavadefinitivo/MainActivity.java
+++ app/src/main/java/cibertec/elequipo/napolipizzjavadefinitivo/MainActivity.java
@@ -8,18 +8,28 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-//import androidx.room.Room;
-//import cibertec.elequipo.napolipizz.database.AppDatabase;
-//import cibertec.elequipo.napolipizz.database.User;
-//import cibertec.elequipo.napolipizz.database.UserDao;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private EditText emailEditText;
     private EditText passwordEditText;
     private Button loginButton;
-    private TextView forgotPasswordTextView;
     private TextView registerTextView;
-    //private UserDao userDao;
+    private RequestQueue requestQueue;
+    private static final String LOGIN_URL = "http://192.168.1.5/movil/login.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,36 +40,66 @@ public class MainActivity extends AppCompatActivity {
         emailEditText = findViewById(R.id.emailEditText);
         passwordEditText = findViewById(R.id.passwordEditText);
         loginButton = findViewById(R.id.loginButton);
-        forgotPasswordTextView = findViewById(R.id.forgotPasswordTextView);
         registerTextView = findViewById(R.id.registerTextView);
 
-        // Inicializar base de datos
-        /*AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "my-database")
-                .allowMainThreadQueries() // Solo para fines de prueba, no lo uses en producción
-                .build();
-        userDao = db.userDao();*/
-
+        requestQueue = Volley.newRequestQueue(this);
         // Configurar click listeners
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String email = emailEditText.getText().toString();
-                String password = passwordEditText.getText().toString();
+                final String email = emailEditText.getText().toString().trim();
+                final String password = passwordEditText.getText().toString().trim();
 
-                // Verificar las credenciales en la base de datos
-                /*User user = userDao.getUserByEmail(email);
-                if (user != null && user.getPassword().equals(password)) {
-                    // Credenciales válidas, iniciar la siguiente actividad
-                    Intent intent = new Intent(MainActivity.this, MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                } else {
-                    // Credenciales inválidas, mostrar mensaje de error
-                    Toast.makeText(MainActivity.this, "Credenciales inválidas", Toast.LENGTH_SHORT).show();
-                }*/
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, LOGIN_URL,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                try {
+                                    JSONArray jsonArray = new JSONArray(response);
+                                    if (jsonArray.length() > 0) {
+                                        // Credenciales válidas, iniciar la siguiente actividad
+                                        Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    } else {
+                                        // Credenciales inválidas, mostrar mensaje de error
+                                        Toast.makeText(MainActivity.this, "Credenciales inválidas", Toast.LENGTH_SHORT).show();
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                // Error en la solicitud, mostrar mensaje de error
+                                Toast.makeText(MainActivity.this, "Error de conexión", Toast.LENGTH_SHORT).show();
+                            }
+                        }) {
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String, String> params = new HashMap<>();
+                        params.put("CorreoElectronico", email);
+                        params.put("Contraseña", password);
+                        return params;
+                    }
+                };
+
+                requestQueue.add(stringRequest);
             }
         });
 
-        // ...
+        registerTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Manejar el evento de "Registrate"
+                // Puedes implementar la lógica para dirigir al usuario al registro aquí
+                Toast.makeText(MainActivity.this, "Registrate", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
